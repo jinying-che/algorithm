0aa4770c9ab0c1de378f8517ab0a4debff994354
Jinying Che <chejinying@me.com>
2023-03-01 00:23:25 +0800

    finishe the heap sort

--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -1,18 +1,22 @@
 def sort(arr):
-    pass
+    return heap_sort(arr)
 
 
 # build heap in arr
-def build(arr):
+def heap(arr):
+    '''
+    heapify all the non-leaf nodes
+    `n // 2 - 1` is the last non-leaf node in the heap (key point)
+    '''
     n = len(arr)
-    for i in range(n//2, -1, -1):
+    for i in range(n//2 - 1, -1, -1):
         heapify(arr, n, i)
     return arr
 
 
 def heapify(arr, n, i):
     '''
-    heapify make sure the sub-heap
+    heapify make sure the tree rooted at i is a max-heap (from top to bottom)
     - arr: input array
     - n: the length of the array
     - i: the index of the current node which is the root of the current heap
@@ -26,21 +30,29 @@ def heapify(arr, n, i):
         largest = right
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
+        # once swapped, heapify to make sure the child heap is a max-heap
         heapify(arr, n, largest)
     return arr
 
 
 def heap_sort(arr):
+    '''
+    1. build a max-heap within the array
+    2. swap the root (max value) with the last leaf node (pick up the largest)
+    3. heapify the rest array
+    3. repeatily 2 && 3 until the start of the array
+    '''
     n = len(arr)
-    arr = build(arr)
+    arr = heap(arr)
     for i in range(n - 1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, n - 1, 0)
+        heapify(arr, i, 0)
+        print(f"{i}, arr{arr}")
     return arr
 
 
 if __name__ == "__main__":
     arr = [1, 2, 4, 5, 8]
     print(f"before: {arr}")
-    print(f"heapify: {build(arr)}")
+    print(f"heapify: {heap(arr)}")
     print(f"heap sort: {heap_sort(arr)}")
--- sorting/test_sort_using_pytest.py
+++ sorting/test_sort_using_pytest.py
@@ -2,6 +2,7 @@ import insert_sort
 import quick_sort
 import merge_sort
 import merge_sort_with_less_space
+import heap_sort
 
 
 test_cases = [
@@ -30,3 +31,7 @@ def test_merge_sort():
 
 def test_merge_sort_with_less_space():
     helper(merge_sort_with_less_space)
+
+
+def test_heap_sort():
+    helper(heap_sort)
